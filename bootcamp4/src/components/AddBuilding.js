76309fcd54e1eee7c29583397dfe46d6b3c0923c
kwilkinson1 <42629195+kwilkinson1@users.noreply.github.com>
2019-11-04 23:00:05 -0500

    Update AddBuilding.js

--- bootcamp4/src/components/AddBuilding.js
+++ bootcamp4/src/components/AddBuilding.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {Button} from 'react-bootstrap';
 
 class AddBuilding extends React.Component {
@@ -17,18 +18,24 @@ class AddBuilding extends React.Component {
             }
         };
 
+        var frm = document.getElementById("listingForm");
+        frm.reset();
+
         const newList = this.props.data.concat(newListing);
         console.log(newList);
+
         newList.sort((a,b) => {
-            if(a.code < b.code) {
-                return -1;
-              }
-              if (a.code > b.code){
-                return 1;
-              }
-              if (a.code === b.code) {
+            var nameA = a.code.toUpperCase(); // ignore upper and lowercase
+            var nameB = b.code.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                     return 1;
+                }
+
+                // names must be equal
                 return 0;
-              }
         });
 
         this.props.listingsUpdate(newList);
@@ -36,40 +43,39 @@ class AddBuilding extends React.Component {
 
     render () {
         return (
-            <div class="border">
-                <div class="bg-secondary text-white" >
-                <span class="border">
-                    <form>
-                        <div class="form-group">
-                            <label for="Code">Code: </label>
-                            <input type="text" class="form-control" id="Code" placeholder="Building code" ref={(data) => this.code = data}></input>
+            <div className="border">
+                <div className="bg-secondary text-white" >
+                <span className="border">
+                    <form id="listingForm">
+                        <h2 style={{textAlign:'center'}}>Add a Listing</h2>
+                        <div className="form-group" style={{padding:'0px 15px', margin:'0px'}}>
+                            <label htmlFor="Code" style={{padding:'0px 15px', margin:'0px'}}>Code: </label>
+                            <input type="text" className="form-control-sm" id="Code" placeholder="Building code" ref={(data) => this.code = data} style={{height:'2rem'}}></input>
                         </div>
-                        <div class="form-group">
-                            <label for="Name">Building Name: </label>
-                            <input type="text" class="form-control" id="Name" placeholder="Building name" ref={(data) => this.buildingName = data}></input>
+                        <div className="form-group" style={{padding:'0px 15px', margin:'0px'}}>
+                            <label htmlFor="Name" style={{padding:'0px 15px', margin:'0px'}}>Building Name: </label>
+                            <input type="text" className="form-control-sm" id="Name" placeholder="Building name" ref={(data) => this.buildingName = data} style={{height:'2rem'}}></input>
                         </div>
-                        <div class="form-group">
-                            <label for="Address">Address: </label>
-                            <input type="text" class="form-control" id="Address" placeholder="Building Address" ref={(data) => this.buildingAddress = data}></input>
+                        <div className="form-group" style={{padding:'0px 15px', margin:'0px'}}>
+                            <label htmlFor="Address" style={{padding:'0px 15px', margin:'0px'}}>Address: </label>
+                            <input type="text" className="form-control-sm" id="Address" placeholder="Building Address" ref={(data) => this.buildingAddress = data} style={{height:'2rem'}}></input>
                         </div>
-                        <div class="form-group">
-                            <label for="Latitude">Latitude: </label>
-                            <input type="text" class="form-control" id="Latitude" placeholder="latitude" ref={(data) => this.latitude = data}></input>
+                        <div className="form-group" style={{padding:'0px 15px', margin:'0px'}}>
+                            <label htmlFor="Latitude" style={{padding:'0px 15px', margin:'0px'}}>Latitude: </label>
+                            <input type="text" className="form-control-sm" id="Latitude" placeholder="latitude" ref={(data) => this.latitude = data} style={{height:'2rem'}}></input>
                         </div>
-                        <div class="form-group">
-                            <label for="Longitude">Longitude: </label>
-                            <input type="text" class="form-control" id="Longitude" placeholder="longitude" ref={(data) => this.longitude = data}></input>
+                        <div className="form-group" style={{padding:'0px 15px', margin:'0px'}}>
+                            <label htmlFor="Longitude" style={{padding:'0px 15px', margin:'0px'}}>Longitude: </label>
+                            <input type="text" className="form-control-sm" id="Longitude" placeholder="longitude" ref={(data) => this.longitude = data} style={{height:'2rem'}}></input>
                         </div>
-                        <div className="add-btn">
-                                <Button 
+                        <Button  style={{padding: '15px'}}
                                 variant="primary"  
                                 size="lg" 
                                 block
                                 onClick={this.addListing.bind(this)}
                                 >
-                                    Add Building
+                                    Add Listing
                                 </Button>
-                        </div>
                     </form>
                 </span>
                 </div>
